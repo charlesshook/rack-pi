99fe98dfbfdd726531ebf0ddc844e1d85962c644
Charles Shook <31199031+charlesshook@users.noreply.github.com>
2020-09-30 23:23:58 -0500

    Show stats on the display

--- displayStats.py
+++ displayStats.py
@@ -2,6 +2,13 @@ from gpiozero import Button
 from gpiozero import LED
 from subprocess import check_call
 from signal import pause
+import time
+import subprocess
+from board import SCL, SDA
+import busio
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_ssd1306
+import psutil
 
 def shutdown():
     check_call(['sudo', 'shutdown', 'now'])
@@ -17,6 +24,48 @@ def main():
 
     button.when_released = reboot
     button.when_held = shutdown
+
+    i2c = busio.I2C(SCL, SDA)
+    disp = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, addr=0x3c)
+
+    disp.fill(0)
+    disp.show()
+
+    width = disp.width
+    height = disp.height
+    image = Image.new("1", (width, height))
+
+    draw = ImageDraw.Draw(image)
+
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+    padding = -2
+    top = padding
+    bottom = height - padding
+    x = 0
+
+    font = ImageFont.load_default()
+
+    while True:
+
+        draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+        cmd = "hostname"
+        HOSTNAME =  subprocess.check_output(cmd, shell = True)
+        cmd = "hostname -I | cut -d\' \' -f1"
+        IP = subprocess.check_output(cmd, shell = True )
+            
+        CPU = "{:3.0f}".format(psutil.cpu_percent())
+        svmem = psutil.virtual_memory()
+        MemUsage = "{:2.0f}".format(svmem.percent)
+
+        draw.text((x, top),       "NAME: " + HOSTNAME.decode('UTF-8'), font=font, fill=255)
+        draw.text((x, top+12),    "IP  : " + IP.decode('UTF-8'),  font=font, fill=255)
+        draw.text((x, top+24),    "CPU : " + CPU + "% | MEM: " + MemUsage + "%", font=font, fill=255)
+
+        disp.image(image)
+        disp.show()
+        time.sleep(1)
 
     pause()
 
